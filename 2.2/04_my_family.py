dc0f088bb79a50db5bedb6cc54a3a117a512d0b0
vas-ide <vas.ide2022@gmail.com>
2022-04-15 16:56:23 +0300

    finish 4 exis

--- 2.2/04_my_family.py
+++ 2.2/04_my_family.py
@@ -10,7 +10,7 @@ my_family = []
 # список списков приблизителного роста членов вашей семьи
 my_family_height = [
     # ['имя', рост],
-    [],
+    # [],
 ]
 
 # Выведите на консоль рост отца в формате
@@ -24,6 +24,13 @@ my_family.append('Leonid')
 my_family.append('Tanya')
 my_family.append('Father')
 
-
+my_family_height.append(['Alex', 172])
+my_family_height.append(['KSY', 162])
+my_family_height.append(['Leonid', 172])
+my_family_height.append(['Tanya', 162])
+my_family_height.append(['Father', 175])
+sum = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1] + my_family_height[4][1]
+print(f'Рост отца - {my_family_height[4][1]} см')
+print(f'Общий рост моей семьи - {sum} см ')
 print(my_family)
 print(my_family_height)
