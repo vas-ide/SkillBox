34cdf7342251e73190c9219e3f35569ecb4d9467
vas-ide <vas.ide2022@gmail.com>
2022-04-15 15:57:50 +0300

    UPD ALL

---
+++ 2.2/04_my_family.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Создайте списки:
+
+# моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
+my_family = []
+
+
+# список списков приблизителного роста членов вашей семьи
+my_family_height = [
+    # ['имя', рост],
+    [],
+]
+
+# Выведите на консоль рост отца в формате
+#   Рост отца - ХХ см
+
+# Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
+#   Общий рост моей семьи - ХХ см
+my_family.append('Alex')
+my_family.append('KSY')
+my_family.append('Leonid')
+my_family.append('Tanya')
+my_family.append('Father')
+
+
+print(my_family)
+print(my_family_height)
