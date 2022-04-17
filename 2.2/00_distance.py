64962d4ae3a7e0f5066364c3941c9f6a75e0f088
vas-ide <vas.ide2022@gmail.com>
2022-04-17 16:36:58 +0300

    Finished

--- 2.2/00_distance.py
+++ 2.2/00_distance.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Есть словарь координат городов
+from pprint import pprint
 
 sites = {
     'Moscow': (550, 370),
@@ -13,10 +14,30 @@ sites = {
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
 distances = {}
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** 0.5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2 ) ** 0.5
+london_paris = ((london[0] - paris[0]) ** 2 + (london[1] - paris[1]) ** 2 ) ** 0.5
+distances ['Moscow'] = {}
+distances ['Moscow']['London'] = moscow_london 
+distances ['Moscow']['Paris'] = moscow_paris
+
+distances ['London'] = {}
+distances ['London']['Moscow'] = moscow_london
+distances ['London']['Paris'] = london_paris
+
+distances ['Paris'] = {}
+distances ['Paris']['London'] = london_paris
+distances ['Paris']['Moscow'] = moscow_paris
+
+
 
 # TODO здесь заполнение словаря
 
-print(distances)
+pprint(distances)
 
 
 
--- 2.2/04_my_family.py
+++ 2.2/04_my_family.py
@@ -32,5 +32,5 @@ my_family_height.append(['Father', 175])
 sum = my_family_height[0][1] + my_family_height[1][1] + my_family_height[2][1] + my_family_height[3][1] + my_family_height[4][1]
 print(f'Рост отца - {my_family_height[4][1]} см')
 print(f'Общий рост моей семьи - {sum} см ')
-print(my_family)
+print(my_family) 
 print(my_family_height)
