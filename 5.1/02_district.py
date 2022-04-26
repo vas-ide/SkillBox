8860e00dbbb3a90753e18b6380e62922a300c620
alex <vas.ide2022@gmail.com>
2022-04-26 12:53:23 +0300

    4 week break 5 week 1-2 finished

--- 5.1/02_district.py
+++ 5.1/02_district.py
@@ -6,7 +6,32 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
 # TODO здесь ваш код
+from pprint import pprint
+
+from district.central_street.house1 import room1 as c_h1_r1, room2 as c_h1_r2
+from district.central_street.house2 import room1 as c_h2_r1, room2 as c_h2_r2
+from district.soviet_street.house1 import room1 as s_h1_r1, room2 as s_h1_r2
+from district.soviet_street.house2 import room1 as s_h2_r1, room2 as s_h2_r2
+
+all_peoples =[]
+
+all_peoples.append(','.join(c_h1_r1.folks))
+all_peoples.append(','.join(c_h1_r2.folks))
+all_peoples.append(','.join(c_h2_r1.folks))
+all_peoples.append(','.join(c_h2_r2.folks))
+
+all_peoples.append(','.join(s_h1_r1.folks))
+all_peoples.append(','.join(s_h1_r2.folks))
+all_peoples.append(','.join(s_h2_r1.folks))
+all_peoples.append(','.join(s_h2_r2.folks))
 
 
+for _ in all_peoples:
+    if bool(_) == False:
+        all_peoples.remove(_)
 
+all_peoples_upd = []
+all_peoples_upd.append(','.join(all_peoples))
+
+print('На районе живут:', *all_peoples_upd)
 
