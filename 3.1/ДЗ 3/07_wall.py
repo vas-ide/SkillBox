f45a81d88321a5468d17a286d5d11f05df778479
alex <vas.ide2022@gmail.com>
2022-04-27 15:13:26 +0300

    4 week finished 5 week 1-2-3finished

--- 3.1/ДЗ 3/07_wall.py
+++ 3.1/ДЗ 3/07_wall.py
@@ -6,9 +6,12 @@ from simple_draw import lines
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
+import simple_draw as sd
 # TODO здесь ваш
 
-resolution = (800, 600)
+sd.resolution = (1200, 600)
+
+wall = (1200, 600)
 x = -100
 y = -100
 
@@ -16,7 +19,7 @@ for _ in range(50):
     if _ % 2 == 0:
         x = -100
         point_start = simple_draw.get_point(x, y)
-        point_end = simple_draw.get_point(resolution[1], y)
+        point_end = simple_draw.get_point(wall[0], y)
         simple_draw.line(point_start, point_end, width=2)
         y += 50
         for __ in range(50):
@@ -27,7 +30,7 @@ for _ in range(50):
     else:
         x = -50
         point_start = simple_draw.get_point(x, y)
-        point_end = simple_draw.get_point(resolution[1], y)
+        point_end = simple_draw.get_point(wall[0], y)
         simple_draw.line(point_start, point_end, width=2)
         y += 50
         for __ in range(50):
--- 4.1/05_snowfall.py
+++ 4.1/05_snowfall.py
@@ -2,14 +2,10 @@
 
 import simple_draw as sd
 
-
 # На основе кода из практической части реализовать снегопад:
 # - создать списки данных для отрисовки N снежинок
 # - нарисовать падение этих N снежинок
 # - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
-
-N = 20
-
 # Пригодятся функции
 # sd.get_point()
 # sd.snowflake()
@@ -18,15 +14,42 @@ N = 20
 # sd.user_want_exit()
 
 # TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+sd.resolution = (1200, 800)
+N = 20
+def snowfall_initional():
+    def snowdrift():
+        left_bottom = sd.get_point(100, -150)
+        right_top = sd.get_point(550, 100)
+        sd.ellipse(left_bottom, right_top, color=sd.COLOR_WHITE)
 
+    dict_x_snowfall = {}
+    dict_y_snowfall = {}
+    dict_len_snowfall = {}
+    for _ in range(N):
+        dict_x_snowfall[_] = sd.random_number(100, 1000)
+        dict_y_snowfall[_] = sd.random_number(100, 1000)
+        dict_len_snowfall[_] = sd.random_number(10, 100)
+
+    while True:
+        sd.clear_screen()
+        sd.start_drawing()
+        snowdrift()
+        for _ in range(N):
+            x = dict_x_snowfall[_]
+            y = dict_y_snowfall[_]
+            length = dict_len_snowfall[_]
+            start_point = sd.get_point(x, y)
+            sd.snowflake(center=start_point, length=length)
+            dict_x_snowfall[_] += sd.random_number(-15, 20)
+            dict_y_snowfall[_] -= 5
+        sd.finish_drawing()
+        sd.sleep(0.1)
+        if y < -5:
+            break
+        if sd.user_want_exit():
+            break
+
+snowfall_initional()
 sd.pause()
 
 # подсказка! для ускорения отрисовки можно
