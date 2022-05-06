d73edfd327ef12c4eaf70f7d47ac02a901cd41dc
alex <vas.ide2022@gmail.com>
2022-05-06 12:59:41 +0300

    4 week need UPG snoow + 5 week upg house and rainbow 6 week 2 ex i progres

--- 6.1/02_snowfall_module.py
+++ 6.1/02_snowfall_module.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
-
+from snowfall import generate_color, generate_snowflakes, touch_snowflakes
+sd.resolution = (1200, 600)
 # На основе кода из lesson_004/05_snowfall.py
 # сделать модуль snowfall.py в котором реализовать следующие функции
 #  создать_снежинки(N) - создает N снежинок
@@ -14,9 +15,17 @@ import simple_draw as sd
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
 # создать_снежинки(N)
+generate_snowflakes(150)
+generate_color()
 while True:
+    # generate_color()
     #  нарисовать_снежинки_цветом(color=sd.background_color)
+    sd.clear_screen()
+    sd.start_drawing()
+    touch_snowflakes()
+    sd.finish_drawing()
     #  сдвинуть_снежинки()
+    # generate_color()
     #  нарисовать_снежинки_цветом(color)
     #  если есть номера_достигших_низа_экрана() то
     #       удалить_снежинки(номера)
