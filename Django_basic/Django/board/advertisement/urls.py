1a01c790e3388f2f9a244bae6b74f63debece5a7
VAS-IDE <vas.ide2022@gmail.com>
2022-10-26 14:29:18 +0300

    Django: init: Middleware & html with python

--- Django_basic/Django/board/advertisement/urls.py
+++ Django_basic/Django/board/advertisement/urls.py
@@ -3,8 +3,8 @@ from .import views
 
 
 urlpatterns = [
-    # path("", views.advertisement_list, name='advertisement_list'),
-    # path("", views.get_client_ip, name='advertisement_list'),
+    path("", views.advertisement_list, name='advertisement_list'),
+    path("", views.get_client_ip, name='advertisement_list'),
     path("advertisement/",  views.advertisement_upd,  name='advertisement_upd'),
     path("advertisement/", views.get_client_ip, name='advertisement_upd'),
     path("repair/", views.repair, name='repair'),
@@ -12,4 +12,9 @@ urlpatterns = [
     path("plumber/", views.plumber, name='plumber'),
     path("construction/", views.construction, name='constructional'),
     path("excavator/", views.excavator, name='excavator'),
+
+    path("contacts/", views.contacts, name='contacts'),
+    path("about/", views.about, name='about'),
+    path("categories/", views.categories, name='categories'),
+    path("region/", views.region, name='region'),
 ]
--- Django_basic/Django/board/advertisement/views.py
+++ Django_basic/Django/board/advertisement/views.py
@@ -9,11 +9,28 @@ def get_client_ip(request):
 
 
 def advertisement_list(request, *args, **kwargs):
-    return render(request, 'advertisement/advertisement_list.html', {})
+    advertisements = [
+        "Мастер по ремонту бытовой техники.",
+        "Ветеринар.",
+        "Сантехник.",
+        "Строительные работы",
+        "Экскаватор",
+        "Уборка дома",
+        "Сад и ландшафтный дизайн",
+        "Погрузочно-разгрузочные работы",
+    ]
+    return render(request, 'advertisement/advertisement_list.html', {'advertisements': advertisements})
 
 
 def advertisement_upd(request, *args, **kwargs):
-    return render(request, 'advertisement/advertisement_upd.html', {})
+    advertisements = [
+        "Мастер по ремонту бытовой техники.",
+        "Ветеринар.",
+        "Сантехник.",
+        "Строительные работы",
+        "Экскаватор",
+    ]
+    return render(request, 'advertisement/advertisement_upd.html', {'advertisements': advertisements})
 
 
 def repair(request, *args, **kwargs):
@@ -34,3 +51,31 @@ def construction(request, *args, **kwargs):
 
 def excavator(request, *args, **kwargs):
     return render(request, 'advertisement/excavator.html', {})
+
+
+def contacts(request, *args, **kwargs):
+    return render(request, 'contacts/contacts.html', {})
+
+
+def about(request, *args, **kwargs):
+    return render(request, 'about/about.html', {})
+
+
+def categories(request, *args, **kwargs):
+    categories = [
+        "Cтроительство",
+        "Ремонт",
+        "Ветеринария",
+        "Медицинская помощь",
+        "Обслуживание дома",
+        "Перевозки",
+    ]
+    return render(request, 'categories/categories.html', {'categories': categories})
+
+
+def region(request, *args, **kwargs):
+    region = [
+        "Краснодарский край.",
+        "Ростовская область",
+    ]
+    return render(request, 'region/region.html', {'region': region})
