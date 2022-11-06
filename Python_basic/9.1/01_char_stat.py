73f0b57ccc7ee900dc7a65fdc19ae6296b0fc268
VAS-IDE <vas.ide2022@gmail.com>
2022-11-06 16:54:31 +0300

    restart - skillbox 09
    1- in progres

--- Python_basic/9.1/01_char_stat.py
+++ Python_basic/9.1/01_char_stat.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import zipfile
+
 
 # Подсчитать статистику по буквам в романе Война и Мир.
 # Входные параметры: файл для сканирования
@@ -23,6 +25,79 @@
 
 # TODO здесь ваш код
 
+class Getstat:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    def collect(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        with open(self.file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                self._collect_for_line(line=line[:-1])
+
+    def _collect_for_line(self, line):
+        for char in line:
+            if char.isalpha():
+                if char in self.stat:
+                    self.stat[char] += 1
+                else:
+                    self.stat[char] = 1
+
+
+voina_stat = Getstat(file_name="voyna-i-mir.txt.zip")
+voina_stat.collect()
+dict_stat = voina_stat.stat
+# sorted_tuple = sorted(dict_stat.items(), key=lambda x: x[0]) # Алфавит
+# sorted_tuple = sorted(dict_stat.items(), key=lambda x: x[0], reverse=True) # Алфавит в обратном порядке
+sorted_tuple = sorted(dict_stat.items(), key=lambda x: x[1], reverse=True) # ТОТАЛ
+dict_stat_sorted = dict(sorted_tuple)
+# dict_stat_revers = sorted(dict_stat, reverse=True)
+
+
+dict_alphabet = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0,
+                 "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0,
+                 "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0,
+                 "y": 0, "z": 0,
+                 "A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0,
+                 "I": 0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0, "O": 0, "P": 0,
+                 "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0,
+                 "Y": 0, "Z": 0}
+dict_alphabet_reverse = {"z": 0, "y": 0, "x": 0, "w": 0, "v": 0, "u": 0, "t": 0, "s": 0,
+                         "r": 0, "q": 0, "p": 0, "o": 0, "n": 0, "m": 0, "l": 0, "k": 0,
+                         "j": 0, "i": 0, "h": 0, "g": 0, "f": 0, "e": 0, "d": 0, "c": 0,
+                         "b": 0, "a": 0,
+                         "Z": 0, "Y": 0, "X": 0, "W": 0, "V": 0, "U": 0, "T": 0, "S": 0,
+                         "R": 0, "Q": 0, "P": 0, "O": 0, "N": 0, "M": 0, "L": 0, "K": 0,
+                         "J": 0, "I": 0, "H": 0, "G": 0, "F": 0, "E": 0, "D": 0, "C": 0,
+                         "B": 0, "A": 0}
+
+
+
+def printer(dict):
+    print(f"+---------+---------+")
+    print(f"|  буква  | частота |")
+    print(f"+---------+---------+")
+    # print(dict_stat)
+    tootal = 0
+    # for key, value in dict_stat.items():
+    #     print(f"|{key:^9}|{value:^9}|")
+    #     tootal += value
+    for key, value in dict.items():
+        print(f"|{key:^9}|{value:^9}|")
+        tootal += value
+    print(f"+---------+---------+")
+    print(f"|  итого  |{tootal:^9}|")
+    print(f"+---------+---------+")
+printer(dict_stat_sorted)
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
 #  - по алфавиту по возрастанию
