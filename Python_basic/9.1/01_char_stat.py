a1b365fee2ce09ccef040542e49cab3604e0ea0f
VAS-IDE <vas.ide2022@gmail.com>
2022-11-06 18:44:53 +0300

    restart - skillbox 09
    Statistic - ready

--- Python_basic/9.1/01_char_stat.py
+++ Python_basic/9.1/01_char_stat.py
@@ -27,7 +27,7 @@ import zipfile
 
 class Getstat:
 
-    def __init__(self, file_name, sort_statistic=None):
+    def __init__(self, file_name, sort_statistic="total"):
         self.file_name = file_name
         self.stat = {}
         self.stat_sort = {}
@@ -59,8 +59,8 @@ class Getstat:
             self.stat_sort = dict(sorted(self.stat.items(), key=lambda x: x[0]))                # Алфавит
         elif self.sort_statistic == "alphabet-r":
             self.stat_sort = dict(sorted(self.stat.items(), key=lambda x: x[0], reverse=True))  # Алфавит в обратном порядке
-        elif self.sort_statistic == "total":
-            self.stat_sort = dict(sorted(self.stat.items(), key=lambda x: x[1], reverse=False)) # ТОТАЛ
+        elif self.sort_statistic == "total-r":
+            self.stat_sort = dict(sorted(self.stat.items(), key=lambda x: x[1], reverse=False)) # ТОТАЛ в обратном порядке
         else:
             self.stat_sort = dict(sorted(self.stat.items(), key=lambda x: x[1], reverse=True))  # ТОТАЛ
 
@@ -74,25 +74,7 @@ voina_stat = Getstat(file_name="voyna-i-mir.txt.zip",
 voina_stat.collect()
 voina_stat.sort_stat()
 dict_stat = voina_stat.stat_sort
-# dict_stat_sorted = dict(sorted(dict_stat.items(), key=lambda x: x[0]))               # Алфавит
-# dict_stat_sorted = dict(sorted(dict_stat.items(), key=lambda x: x[0], reverse=True)) # Алфавит в обратном порядке
-# dict_stat_sorted = dict(sorted(dict_stat.items(), key=lambda x: x[1], reverse=True)) # ТОТАЛ
-# dict_alphabet = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0,
-#                  "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0,
-#                  "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0,
-#                  "y": 0, "z": 0,
-#                  "A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0,
-#                  "I": 0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0, "O": 0, "P": 0,
-#                  "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0,
-#                  "Y": 0, "Z": 0}
-# dict_alphabet_reverse = {"z": 0, "y": 0, "x": 0, "w": 0, "v": 0, "u": 0, "t": 0, "s": 0,
-#                          "r": 0, "q": 0, "p": 0, "o": 0, "n": 0, "m": 0, "l": 0, "k": 0,
-#                          "j": 0, "i": 0, "h": 0, "g": 0, "f": 0, "e": 0, "d": 0, "c": 0,
-#                          "b": 0, "a": 0,
-#                          "Z": 0, "Y": 0, "X": 0, "W": 0, "V": 0, "U": 0, "T": 0, "S": 0,
-#                          "R": 0, "Q": 0, "P": 0, "O": 0, "N": 0, "M": 0, "L": 0, "K": 0,
-#                          "J": 0, "I": 0, "H": 0, "G": 0, "F": 0, "E": 0, "D": 0, "C": 0,
-#                          "B": 0, "A": 0}
+
 
 def printer(dict):
     print(f"+---------+---------+")
