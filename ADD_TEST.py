e1961076339f40f0606318336b5d2a459ac38e8f
VAS-IDE <vas.ide2022@gmail.com>
2022-11-06 19:10:22 +0300

    restart - skillbox 09
    Statistic - ready
    Analize - start

--- ADD_TEST.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-
-file_name = 'pushkin.txt'
-file = open(file_name, mode='rb')  # mode (режим): чтение бинарное
-file_content = file.read()
-file.close()
-pprint(file_content)
--- Python_basic/9.1/02_log_parser.py
+++ Python_basic/9.1/02_log_parser.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import zipfile
+
 
 # Имеется файл events.txt вида:
 #
@@ -20,6 +22,35 @@
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
 # TODO здесь ваш код
+class Analize:
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.stat = {}
+
+    def unzip(self):
+        zfile = zipfile.ZipFile(self.file_name, 'r')
+        for filename in zfile.namelist():
+            zfile.extract(filename)
+        self.file_name = filename
+
+    # def collect(self):
+    #     if self.file_name.endswith('.zip'):
+    #         self.unzip()
+    #     with open(self.file_name, 'r', encoding='cp1251') as file:
+    #         for line in file:
+    #             self._collect_for_line(line=line[:-1])
+    #
+    # def _collect_for_line(self, line):
+    #     for char in line:
+    #         if self.sequence in self.stat:
+    #             if char in self.stat[self.sequence]:
+    #                 self.stat[self.sequence][char] += 1
+    #             else:
+    #                 self.stat[self.sequence][char] = 1
+    #         else:
+    #             self.stat[self.sequence] = {char: 1}
+    #         self.sequence = self.sequence[1:] + char
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
