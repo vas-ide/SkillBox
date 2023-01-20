523ab1597324c2ab957588995c90df67c80e162e
VAS-IDE-PC <vas.ide2022@gmail.com>
2023-01-20 15:00:53 +0300

    PY - HOME  +++    - Scientific Expedition  init:
             - 6   mastermaind - 20.01.23  repair - actual knowledge
    SkillBox - 10  1-ended 2-ended 3-ended
             - 11  Init adn rename all

--- Python_basic/6/01_mastermind.py
+++ Python_basic/6/01_mastermind.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from pprint import pprint
 
 # Игра «Быки и коровы»
 # https://goo.gl/Go2mb9
--- Python_basic/6/mastermind_engine.py
+++ Python_basic/6/mastermind_engine.py
@@ -18,6 +18,17 @@ def generate_number():
             generate_number_str += str(num)
     return generate_number_str
 
+def check_a_generated_number(input_number, generate_number):
+    cows_num = 0
+    bulls_num = 0
+    for num_g, value_g in enumerate(generate_number):
+        for num_i, value_i in enumerate(input_number):
+             if num_g == num_i and value_g == value_i:
+                 bulls_num += 1
+             elif value_g == value_i:
+                 cows_num += 1
+    return print(colored(f'Быки - {bulls_num}  Коровы - {cows_num}', color='cyan'))
+
 
 
 
@@ -42,25 +53,26 @@ def generate_number():
 #     return generate_number_str
 
 
-def check_a_generated_number(input_number=1234, generate_number=1234):
-    input_number = str(input_number)
-    generate_number = str(generate_number)
-    generate_number_dict = {}
-    input_number_dict = {}
-    cows_num = 0
-    bulls_num = 0
-    livestock = {'bulls': 0, 'cows': 0}
-    for num, value in enumerate(generate_number):
-        generate_number_dict[num] = value
-    for num, value in enumerate(input_number):
-        input_number_dict[num] = value
-    for key in range(len(generate_number)):
-        if generate_number_dict[key] == input_number_dict[key]:
-            bulls_num += 1
-    livestock['bulls'] = bulls_num
-    for i in generate_number:
-        for j in input_number:
-            if i == j:
-                cows_num += 1
-    livestock['cows'] = cows_num - bulls_num
-    return print(colored(f'Быки - {livestock["bulls"]}  Коровы - {livestock["cows"]}', color='cyan'))
+
+# def check_a_generated_number(input_number=1234, generate_number=1234):
+#     input_number = str(input_number)
+#     generate_number = str(generate_number)
+#     generate_number_dict = {}
+#     input_number_dict = {}
+#     cows_num = 0
+#     bulls_num = 0
+#     livestock = {'bulls': 0, 'cows': 0}
+#     for num, value in enumerate(generate_number):
+#         generate_number_dict[num] = value
+#     for num, value in enumerate(input_number):
+#         input_number_dict[num] = value
+#     for key in range(len(generate_number)):
+#         if generate_number_dict[key] == input_number_dict[key]:
+#             bulls_num += 1
+#     livestock['bulls'] = bulls_num
+#     for i in generate_number:
+#         for j in input_number:
+#             if i == j:
+#                 cows_num += 1
+#     livestock['cows'] = cows_num - bulls_num
+#     return print(colored(f'Быки - {livestock["bulls"]}  Коровы - {livestock["cows"]}', color='cyan'))
