302e7078af89d06b008a1f66d216d7b4e8870e11
vas-ide <vas.ide2022@gmail.com>
2023-01-26 13:11:14 +0300

    SkillBox:  11 init

--- ADD_TEST.py
+++ ADD_TEST.py
@@ -1,17 +1,5 @@
-class Multiplier:
-
-    def __init__(self, n):
-        self.n = n
-
-    def __call__(self, x):
-        # если есть такой метод у класса - то его обьект можно "вызывать" как функцию
-        return x * self.n
 
 
-my_numbers = [3, 1, 4, 1, 5, 9, 2, 6]
-by_100500 = Multiplier(n=100500)
-result = by_100500(x=42)
-print(result)
 
-result = map(by_100500, my_numbers)
-print(list(result))
+
+
