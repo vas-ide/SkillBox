4272bfe0840bd27abbaa475c7f8b00821f66fcea
vas-ide <vas.ide2022@gmail.com>
2023-01-28 08:20:37 +0300

    SkillBox:  11 init  generation

--- TEST.py
+++ TEST.py
@@ -1,37 +1,7 @@
 
 
-class PrimeNumbers:
-
-    def __init__(self, n):
-        self.n = n
-        self.prime_numbers = []
-
-    def get_prime_numbers(self):
-        for number in range(2, self.n + 1):
-            for prime in self.prime_numbers:
-                if number % prime == 0:
-                    break
-            else:
-                self.prime_numbers.append(number)
-                yield number
 
 
-
-
-
-
-# num = PrimeNumbers(11)
-# num.__call__()
-# print(list(num.__call__()))
-
-prime_number_iterator = PrimeNumbers(n=11).get_prime_numbers()
-for number in prime_number_iterator:
-    print(number)
-    if number == 3:
-        break
-for number in prime_number_iterator:
-    if number > 10:
-        print(number)
 
 
 
