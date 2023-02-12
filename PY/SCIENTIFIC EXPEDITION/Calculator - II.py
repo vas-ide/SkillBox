a94f5ae37b2dcb2d60e3de4fe91a10cfa039fbf2
vas-asus-ide <vas.ide2022@gmail.com>
2023-02-12 16:39:13 +0300

    Py CACL II fix add add_analiz

--- PY/SCIENTIFIC EXPEDITION/Calculator - II.py
+++ PY/SCIENTIFIC EXPEDITION/Calculator - II.py
@@ -68,49 +68,14 @@ def calculator(log: str) -> str:
                                         self.inf_init_upd.append(__)
 
 
-                def init_inf_init_construct(self):
-                    pass
-
-
-
-
-
-
-                # def analize(self):
-                #     if len(self.inf) == 1 and self.inf[0] in self.symbol_lst:
-                #         self.inf_upd.append(0)
-                #     else:
-                #         for _, __ in enumerate(self.inf):
-                #             if len(self.inf) == 1 and __ in self.symbol_lst:
-                #                 self.inf_upd.append(0)
-                #             elif __ == " ":
-                #                 pass
-                #             elif __ in self.symbol_lst and len(self.more_arg) < 1:
-                #                 if __ == "-":
-                #                     self.more_arg += __
-                #                 # else:
-                #                 #     pass
-                #             elif __ not in self.symbol_lst:
-                #                 self.more_arg += __
-                #             elif __ in self.symbol_lst and _ == len(self.inf) - 1:
-                #                 pass
-                #             elif __ in self.symbol_lst and len(self.more_arg) >= 1:
-                #                 self.inf_upd.append(int(self.more_arg))
-                #                 self.inf_upd.append(__)
-                #                 self.more_arg = ""
-                #         self.inf_upd.append(int(self.more_arg))
-                #         if __ in self.symbol_lst and _ == len(self.inf) - 1:
-                #             self.inf_upd.append(__)
-
-
-
+                # def init_inf_init_construct(self):
 
                 def calculation(self):
                     if len(self.inf_upd) == 1:
                         self.result = str(self.inf_upd[0])
                     else:
                         self.result = self.inf_upd[0]
-                        for _, __ in enumerate(self.inf_upd):
+                        for _, __ in enumerate(self.inf_init_upd):
                             if __ == "+" and len(self.inf_upd) >= _ + 3:
                                 if self.inf_upd[_ + 2] in self.symbol_lst:
                                     self.result += self.inf_upd[_ + 1]
@@ -128,10 +93,36 @@ def calculator(log: str) -> str:
                                     self.result = self.inf_upd[_ + 1]
                             if __ == "=" and len(self.inf_upd) == _ + 2:
                                 self.result = self.inf_upd[_ + 1]
+
+
+
+                # def calculation(self):
+                #     if len(self.inf_upd) == 1:
+                #         self.result = str(self.inf_upd[0])
+                #     else:
+                #         self.result = self.inf_upd[0]
+                #         for _, __ in enumerate(self.inf_init_upd):
+                #             if __ == "+" and len(self.inf_upd) >= _ + 3:
+                #                 if self.inf_upd[_ + 2] in self.symbol_lst:
+                #                     self.result += self.inf_upd[_ + 1]
+                #             elif __ == "+" and len(self.inf_upd) >= _ + 2:
+                #                 self.result = self.inf_upd[_ + 1]
+                #             elif __ == "-" and len(self.inf_upd) >= _ + 3:
+                #                 if self.inf_upd[_ + 2] in self.symbol_lst:
+                #                     self.result -= self.inf_upd[_ + 1]
+                #             elif __ == "-" and len(self.inf_upd) >= _ + 2:
+                #                 self.result = self.inf_upd[_ + 1]
+                #             elif __ == "-" and len(self.inf_upd) >= _ + 4:
+                #                 self.result = self.inf_upd[_ + 3]
+                #             elif __ == "=" and len(self.inf_upd) >= _ + 3:
+                #                 if self.inf_upd[_ + 2] in self.symbol_lst:
+                #                     self.result = self.inf_upd[_ + 1]
+                #             if __ == "=" and len(self.inf_upd) == _ + 2:
+                #                 self.result = self.inf_upd[_ + 1]
             cac = CalcInit(infirmation)
             cac.init_analiz()
             cac.addition_analiz()
-            print(cac.inf_init)
+            # print(cac.inf_init)
             print(cac.inf_init_upd)
             # cac.analize()
             # print((f"{cac.inf_upd}"))
