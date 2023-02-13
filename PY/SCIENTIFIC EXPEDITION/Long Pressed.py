e204dd203ec9d7840f92750215a999a8cb0eb5d0
VAS-PC-IDE <vas.ide2022@gmail.com>
2023-02-13 21:17:01 +0300

    long_pressed --- init

--- PY/SCIENTIFIC EXPEDITION/Long Pressed.py
+++ PY/SCIENTIFIC EXPEDITION/Long Pressed.py
@@ -12,8 +12,38 @@ def long_pressed(text: str, typed: str) -> bool:
             else:
                 pass
 
+
+
+
+            # elif len(inf_innit) > len(inf_mod):
+            #     print(f"{False}")
+            #     return False
+            # else:
+            #     res_innit = [None]
+            #     res_mod = [None]
+            #     for _ in inf_innit:
+            #         if _ != res_innit[-1]:
+            #             res_innit.append(_)
+            #     res_innit = "".join(res_innit[1:])
+            #     for _ in inf_mod:
+            #         if _ != res_mod[-1]:
+            #             res_mod.append(_)
+            #     res_mod = "".join(res_mod[1:])
+            #     print(res_innit, res_mod)
+            #     if res_innit == res_mod:
+            #         print(f"{True}")
+            #         return True
+            #     else:
+            #         print(f"{False}")
+            #         return False
+
+
+
         case _:
             print(f"Неправильный  формат данных !")
+
+
+
 
 
 
@@ -25,12 +55,6 @@ long_pressed("there is an error here", "there is an errorrr hereaa")
 # False
 long_pressed("hi, my name is...", "hi, my name is...")
 # False
-
-
-
-
-
-
 
 
 
