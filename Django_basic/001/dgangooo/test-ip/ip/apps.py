4366ed6cd88373518df2565ba9b40c4133f9bd95
vas-ide <vas.ide2022@gmail.com>
2023-03-26 15:51:24 +0300

    Django you

--- Django_basic/001/dgangooo/test-ip/ip/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IpConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ip'
--- Django_basic/001/dgangooo/test-ip/ip/views.py
+++
@@ -1,7 +0,0 @@
-
-from django.shortcuts import render
-
-
-def get_client_ip(request):
-    ip = request.META.get('REMOTE_ADDR')
-    return render(request, "ip/ip.html", {'ip_address': ip})
