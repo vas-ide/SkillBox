a45f133ef2fc74ab1141291b9c22a689e712c5f8
vas-ide <vas.ide2022@gmail.com>
2023-03-26 11:38:46 +0300

    Py list simple

--- Python_basic/11/01_shapes.py
+++ Python_basic/11/01_shapes.py
@@ -14,12 +14,37 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    # Цикл FOR Треугольник
+
+    def triangle(start_point, angle=0, length=150):
+        for i in range(3):
+            v_i = sd.get_vector(start_point=start_point, angle=angle, length=length + 50, width=3)
+            start_point = v_i.end_point
+            angle += 120
+            v_i.draw()
+
+    # Цикл FOR Квадрат
+    def square(start_point=sd.get_point(400, 400), angle=0, length=150):
+        for i in range(4):
+            v_i = sd.get_vector(start_point=start_point, angle=angle, length=length + 50, width=3)
+            start_point = v_i.end_point
+            angle += 90
+            v_i.draw()
+    # Цикл FOR Пятиугольник
+    def pentagon(start_point=sd.get_point(100, 400), angle=0, length=150):
+        for i in range(5):
+            v_i = sd.get_vector(start_point=start_point, angle=angle, length=length, width=3)
+            start_point = v_i.end_point
+            angle += 72
+            v_i.draw()
+    if n == 3:
+        return triangle(sd.get_point(400, 400), angle=0, length=150)
+
+
 
 
 draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
+# draw_triangle(start_point=sd.get_point(200, 200), angle=13, length=100)
 
 
 sd.pause()
