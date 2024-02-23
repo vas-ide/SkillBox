b16a7b47cbe1d2e4d148c574b11c37d6df7a9d14
VAS <vas.ide2022@gmail.com>
2024-02-23 12:22:35 +0300

    Py

---
+++ Python_basic/13/ticket_upd.py
@@ -0,0 +1,36 @@
+
+
+
+
+class TicketInfoAdd:
+
+    def __init__(self):
+        pass
+
+    def image(self):
+        pass
+
+    def add_info(self):
+        pass
+
+    def run(self):
+        pass
+    pass
+
+ticket = TicketInfoAdd()
+ticket.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
