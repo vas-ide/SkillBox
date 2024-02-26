a0e4b0956b6990191a796ce2dc7a8b379292377b
VAS <vas.ide2022@gmail.com>
2024-02-26 17:46:05 +0300

    Skillbox Boot

--- diploma/bowling/src/bowling.py
+++ diploma/bowling/src/bowling.py
@@ -1,22 +1,19 @@
-
-
 class Bowling:
     def __init__(self, init_inf: str):
         self.data = list(init_inf.replace("-", "0"))
         self.data_upd = list(init_inf.replace("-", "0"))
-        # self.symbol_lst = ["X", "/"]
-        self.error = None
         self.total = 0
 
     def run(self):
         self.play()
+        return self.total
 
     def play(self):
         for numb, item in enumerate(self.data):
             if item == "X":
                 self.total += 20
                 self.data_upd.remove(item)
-            elif item == "/":
+            elif item == "/" and numb > 0:
                 self.total += 15
                 self.data_upd.remove(self.data[numb - 1])
                 self.data_upd.remove(item)
@@ -24,19 +21,10 @@ class Bowling:
             try:
                 self.total += int(item)
             except Exception as error:
-                raise ValueError(f"unpredictable symbol")
-                self.error = type(error)
-
-
-
+                raise ValueError(f"Unpredictable symbol check string")
 
 
 def get_score(game_result: str) -> int:
     bowling = Bowling(game_result)
     bowling.run()
-    if bowling.error is None:
-        print(bowling.total)
-        return bowling.total
-    print(bowling.error)
-    return bowling.error
-
+    return bowling.total
