ff138509b03cbf2adac34666390fa61bdcc75317
VAS <vas.ide2022@gmail.com>
2024-03-25 12:51:50 +0300

    Skillbox  Continue

--- Python_basic/models_and_packages/01_dungeon.py
+++ Python_basic/models_and_packages/01_dungeon.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 import json
+import re
 from datetime import time
 
 # С помощью JSON файла rpg.json задана "карта" подземелья.
@@ -58,12 +59,14 @@ class Game:
         self.time_init = 50
         self.time_now = None
         self.time_stop = datetime.timedelta(seconds=10)
-        self.experience = experience
+        self.experience = int(experience)
         self.map = None
         self.monsters_lst = []
         self.monsters_dict = {}
         self.location_lst = []
         self.location_dict = {}
+        self.reg_mon = r"_exp(\d{1,5})_"
+        self.reg_time = r"_tm(\d{1,20})"
         pass
 
     def prepare(self):
@@ -75,48 +78,75 @@ class Game:
     def map_for_game(self):
         with open("rpg.json", "r", encoding='utf8') as read_file:
             self.map = json.load(read_file)
+
     def print_map(self):
         if type(self.map) == dict:
             for key, value in self.map.items():
                 print(f"Вы находитесь в {key}")
                 print(f"Внутри вы видете")
-                print(f"У вас {self.experience} и осталось {self.time_now} cсекунд")
+                print(f"У вас {self.experience} опыта и осталось {self.time_now} cсекунд")
                 print(f"Внитри вы видете:")
-                monsters = [print(f"-- Монстра {item}") for item in value if type(item) == str and item not in self.monsters_lst]
-                [print(f"-- Вход в локацию{item}") for item in value if type(item) == dict]
+                monsters = [print(f"-- Монстра {item}") for item in value if
+                            type(item) == str and item not in self.monsters_lst]
+                locations = [item for item in value if type(item) == dict]
+                for item in value:
+                    if type(item) == dict:
+                        for key_add in item.keys():
+                            print(f"-- Вход в локацию {key_add}")
                 print(f"Выберите действие:")
-                print(f"1.Атаковать монстра")
-                print(f"2.Перейти в другую локацию")
+                if len(monsters) > 0:
+                    print(f"1.Атаковать монстра")
+                if len(locations) > 0:
+                    print(f"2.Перейти в другую локацию")
                 print(f"3.Выход")
-                answer = int(input(f"You solution    "))
-                print(answer)
+                try:
+                    answer = int(input(f"\nYou solution    "))
+                    print(f"")
+                    if answer == 1:
+                        if len(monsters) > 0:
+                            marker = True
+                            for item in value:
+                                if marker == True:
+                                    if type(item) == str and item not in self.monsters_lst:
+                                        self.monsters_lst.append(item)
+                                        exp = re.search(self.reg_mon, item)[0][4:-1]
+                                        self.experience += int(exp)
+                                        time_mon = re.search(self.reg_time, item)[0][3:]
+                                        print(time_mon)
+                                        marker = False
+                        else:
+                            print(f"\nВ этой локации вы победили всех монстров\n")
 
-                if answer == 1:
-                    if len(monsters) > 0:
-                        print("Монстры")
-                        monsters = [print(f"-- {item}") for item in value if
-                         type(item) == str and item not in self.monsters_lst]
-                        [self.monsters_lst.append(item) for item in value if
-                         type(item) == str and item not in self.monsters_lst]
-                    else:
-                        print(f"В этой локации вы победили всех монстров")
+                    elif answer == 2:
+                        if len(locations) > 1:
+                            print(f"Выберите куда хотите переместиться")
+                            for item in value:
+                                if type(item) == dict:
+                                    for key_add in item.keys():
+                                        print(f"-- Вход в локацию {key_add}")
+                            try:
+                                answer_add = int(input(f"\nYou solution    "))
+                                print(f"")
 
-                elif answer == 3:
-                    self.marker = False
+                                if answer_add > len(locations) or type(answer_add) != int:
+                                    print(f"\nУкажите верное число для выбора направления перехода\n")
+                                else:
+                                    marker = True
+                                    counter = 0
+                                    for item in value:
+                                        if type(item) == dict:
+                                            counter += 1
+                                            if counter == answer_add:
+                                                self.map = item
+                            except ValueError:
+                                print(f"Попробуйте еще раз")
+                        else:
+                            self.map = [item for item in value if type(item) == dict]
+                    elif answer == 3:
+                        self.marker = False
+                except ValueError:
+                    print(f"Попробуйте еще раз")
 
-
-
-    # Вы находитесь в Location_0_tm0
-    # У вас 0 опыта и осталось 1234567890.0987654321 секунд
-    # Прошло уже 0:00:00
-    # Внутри вы видите:
-    # -- Монстра Mob_exp10_tm0
-    # -- Вход в локацию: Location_1_tm10400000
-    # -- Вход в локацию: Location_2_tm333000000
-    # Выберите действие:
-    # 1.Атаковать монстра
-    # 2.Перейти в другую локацию
-    # 3.Выход
 
     def run(self):
         self.prepare()
@@ -124,19 +154,7 @@ class Game:
         while self.marker == True:
             self.print_map()
 
-        pass
-
-
-        # data_str = json.load(file)
-        # data_dict = json.loads(file)
-        # [print(key, '\n', value) for key, value in data_str.items()]
-        # [print(key, '\n', value) for key, value in data_dict.items()]
-        # print(data)
-        # for key, value in data.items():
-        #     print(key, "\n", value)
-
 game = Game()
 game.run()
 
 # Учитывая время и опыт, не забывайте о точности вычислений!
-
